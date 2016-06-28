8627c6b707c493bd75739d46651e72750e046b14
Tony Simpson <anthony.simpson@sky.uk>
2016-06-28 13:02:23 +0100

    POC of python 3.5 async + libcurl
    I expect to find that I've done lots of things wrong but as far
    as I can tell this gives very good throughput (> 4K requests/s).
    It is very possible that it will be able to make this faster
    even after fixing all the issues and missing features to make
    it comparable to Requests.

---
+++ loop.py
@@ -0,0 +1,30 @@
+import minimalmite
+import time
+import asyncio
+import logging
+import sys
+import msgpack
+
+controller = minimalmite.SessionController()
+logging.basicConfig()#level=logging.DEBUG)
+	
+
+S = int(sys.argv[1])
+N = int(sys.argv[2])
+
+results_file = open("results_%d_%d.msgpack" % (S, N), "wb")
+packer = msgpack.Packer()
+
+async def test():
+	session = controller.create_new_session(metrics_callback=lambda d: results_file.write(packer.pack(d)))
+	for i in range(N):
+		try:
+			await session.request('http://127.0.0.1:9003')
+		except Exception as e:
+			print(e)
+	print(N / (time.time() - st))
+
+loop = asyncio.get_event_loop()
+st = time.time()
+print(loop.run_until_complete(asyncio.wait([test() for i in range(S)])))
+print("TOTAL:", (S*N) / (time.time() - st))
