fe8478df9860a1ff14fe7b569ddb0ed6ca104b7e
Tony Simpson <anthony.simpson@sky.uk>
2016-09-12 12:18:04 +0100

    Removed duplicate call to _perform

--- minimalmite/session.py
+++ minimalmite/session.py
@@ -133,9 +133,7 @@ class SessionController:
             self._socket_poll_remove(multi, socket)
         else:
             raise ValueError("unhandled event type %r" % event)
-        if not self._perform_running:
-            asyncio.ensure_future(self._perform())
-
+    
     def _finish(self, multi):
         num_msgs, successes, errors = multi.info_read()
         for success in successes:
