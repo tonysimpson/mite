932bb73c7c873967d744e4137e49a70f852e2a67
Jordan Brennan <jb098@hotmail.co.uk>
2017-10-12 13:31:55 +0100

    Merging my mite stuff with Tony's

--- mite/exceptions.py
+++ mite/exceptions.py
@@ -10,3 +10,9 @@ class UnexpectedResponseCodeError(MiteException):
         self.fields['body'] = resp.content
         self.fields['status_code'] = resp.status_code
         self.fields['headers'] = resp.headers
+
+
+class OptionError(MiteException):
+    def __init__(self, value):
+        super().__init__()
+        self.message = "Attempted to set a value not in options".format(value)
