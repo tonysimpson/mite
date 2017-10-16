66262ffd9aab5aa53cf99fbbca78631e8cc4d545
Tony Simpson <anthony.simpson@sky.uk>
2017-10-16 11:42:19 +0100

    WIP more work on controller

--- mite/controller.py
+++ mite/controller.py
@@ -1,11 +1,7 @@
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from itertools import count
 import time
 
-
-class ControllerTransportExample:
-    pass
-    
 
 class workTracker:
     def __init__(self):
@@ -24,17 +20,35 @@ class workTracker:
         for work in self._all_work.values():
             for k, v in work:
                 totals[k] += v
+        return totals
+
+    def get_runner_total(self, runner_id):
+        return sum(self._all_work[runner_id].values())
+
+
+class RunnerTracker:
+    def __init__(self, timeout=10):
+        self._last_seen = {}
+        self._timeout = timeout
+
+    def update(self, runner_id):
+        self._last_seen[runner_id] = time.time()
+
+    def get_active_count(self):
+        t = time.time()
+        return sum(1 for k, v in self._last_seen.items() if v + self._timeout > t)
+
+
+WorkItem = namedtuple('WorkItem', 'journey_spec argument_datapool_id number minimum_duration'.split())
 
 
 class Controller:
-    def __init__(self, testname, scenario_manager, period=10):
+    def __init__(self, testname, scenario_manager):
         self._testname = testname
         self._scenario_manager = scenario_manager
-        self._period = period
         self._runner_id_gen = count(1)
         self._work_tracker = WorkTracker()
-        self._current_period_expire = 0
-        self._required = {} 
+        self._runner_tracker = RunnerTracker()
         self._start_time = time.time()
     
     def initialise(self):
@@ -43,24 +57,53 @@ class Controller:
     def _set_actual(self, runner_id, current_work):
         self._work_tracker.set_actual(runner_id, current_work)
 
+    def _add_sssumed(self, runner_id, work):
+        self._work_tracker.add_assumed(runner_id, work)
+
     def _now(self):
         return time.time() - self._start_time
 
-    def _update_required_if_expired(self):
-        now = self._now()
-        if now > self._current_period_expire:
-            new_period_expire = now + self._period
-            self._required = self._scenario_manager.required(self._current_period_expire, new_period_expire)
-            self._current_period_expire = new_period_expire
+    def _get_current_required_work(self):
+        required = self._scenario_manager.get_required_work()
+        current = self._work_tracker.get_total_work()
+        diff = {}
+        for id, current_num in current.items():
+            if id in required:
+                diff_num = required[id].number - current_num
+                if diff_num > 0:
+                    diff[id] = required[id]._replace(number=diff_num)
+        return diff
 
-    def _calculate_required_work(self):
+    def _sum_workitem_dict(self, wi_dict):
+        total = 0
+        for wi in wi_dict.values():
+            total += wi.number
+        return total
 
-        
+    def _gen_required_work_for_runner(self, runner_id):
+        required = self._get_current_required_work()
+        required_total = self._sum_workitem_dict(required)
+        runner_total = self._work_tracker.get_runner_total(runner_id)
+        active_runners = self._runner_tracker.get_active_count()
+        max_num = max(0, (required_total // active_runners) - runner_total)
+        for id, wi in required:
+            if max_num <= 0:
+                break
+            if max_num < wi.number:
+                yield id, wi._replace(number=max_num)
+                break
+            else:
+                max_num -= wi.number
+                yield id, wi
     
     def work_request(self, runner_id, current_work):
         self._set_actual(runner_id, current_work)
-        return self._calculate_required_work()
+        self._runner_tracker.update(runner_id)
+        work = list(self._get_required_work_for_runner(runner_id))
+        self._add_assumed(runner_id, {id: wi.number for id, wi in work.items()})
+        return [(id, wi.journey_spec, wi.argument_datapool_id, wi.number, wi.minimum_duration) for id, wi in work]
 
-
-
+    def checkin_and_checkout(self, runner_id, datapool_id, used_ids):
+        self._scenario_manager.checkin_block(datapool_id, used_ids)
+        return self._scenario_manager.checkout_block(datapool_id)
 
