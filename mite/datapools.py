33b258697067f5ded023ca9cd5fcca0611475ad4
Tony Simpson <anthony.simpson@sky.uk>
2017-10-31 15:30:55 +0000

    Added IterableFactoryDataPool

--- mite/datapools.py
+++ mite/datapools.py
@@ -20,9 +20,6 @@ class RecyclableIterableDataPool:
         self._checked_out = {}
         self._available = deque(DataPoolItem(id, data) for id, data in enumerate(iterable, 1))
 
-    def size(self):
-        return len(self._checked_out) + len(self._available)
-
     def checkout(self):
         if self._available:
             dpi = self._available.popleft()
@@ -36,13 +33,45 @@ class RecyclableIterableDataPool:
         self._available.append(DataPoolItem(id, data))
 
 
+class IterableFactoryDataPool:
+    def __init__(self, iterable_factory):
+        self._iterable_factory = iterable_factory
+        self._checked_out = set()
+    
+    def _cycle(self):
+        counter = count(1)
+        _iter = iter(self._iterable_factory())
+        while True:
+            try:
+                data = next(_iter)
+            except StopIteration:
+                counter = count(1)
+                _iter = iter(self._iterable_factory())
+                data = next(_iter)
+            _id = next(counter)
+            yield _id, data
+
+    def checkout(self):
+        if not hasattr(self, '_cycle_gen_iter'):
+            self._cycle_gen_iter = self._cycle()
+        last_id = 0
+        while True:
+            _id, data = next(self._cycle_gen_iter)
+            if _id not in self._checked_out:
+                self._checked_out.add(_id)
+                return DataPoolItem(_id, data)
+            if _id < last_id:
+                return None
+            last_id = _id
+    
+    def checkin(self, id):
+        self._checked_out.remove(id)
+
+
 class IterableDataPool:
     def __init__(self, iterable):
         self._iter = iter(iterable)
         self._id_gen = count(1)
-
-    def size(self):
-        return None
 
     def checkout(self):
         try:
